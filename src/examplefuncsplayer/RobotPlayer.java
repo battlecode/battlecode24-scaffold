5d22f0872fa2034cce3d5a62646078c9f20179bd
TheApplePieGod <evant.002@gmail.com>
2024-01-08 14:45:16 -0500

    Add examplefuncsplayer

--- src/examplefuncsplayer/RobotPlayer.java
+++ src/examplefuncsplayer/RobotPlayer.java
@@ -1,6 +1,7 @@
 package examplefuncsplayer;
 
 import battlecode.common.*;
+import battlecode.schema.GameplayConstants;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -55,7 +56,7 @@ public strictfp class RobotPlayer {
 
         // Hello world! Standard output is very useful for debugging.
         // Everything you say here will be directly viewable in your terminal when you run a match!
-        System.out.println("I'm a " + rc.getType() + " and I just got created! I have health " + rc.getHealth());
+        System.out.println("I'm alive");
 
         // You can also use indicators to save debug notes in replays.
         rc.setIndicatorString("Hello world!");
@@ -69,18 +70,58 @@ public strictfp class RobotPlayer {
 
             // Try/catch blocks stop unhandled exceptions, which cause your robot to explode.
             try {
+                // Make sure you spawn your robot in before you attempt to take any actions!
+                // Robots not spawned in do not have vision of any tiles and cannot perform any actions.
+                if (!rc.isSpawned()){
+                    MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+                    // Pick a random spawn location to attempt spawning in.
+                    MapLocation randomLoc = spawnLocs[rng.nextInt(spawnLocs.length)];
+                    if (rc.canSpawn(randomLoc)) rc.spawn(randomLoc);
+                }
+                else{
+                    if (rc.canPickupFlag(rc.getLocation())){
+                        rc.pickupFlag(rc.getLocation());
+                        rc.setIndicatorString("Holding a flag!");
+                    }
+                    // If we are holding an enemy flag, singularly focus on moving towards
+                    // an ally spawn zone to capture it! We use the check roundNum >= SETUP_ROUNDS
+                    // to make sure setup phase has ended.
+                    if (rc.hasFlag() && rc.getRoundNum() >= GameConstants.SETUP_ROUNDS){
+                        MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+                        MapLocation firstLoc = spawnLocs[0];
+                        Direction dir = rc.getLocation().directionTo(firstLoc);
+                        if (rc.canMove(dir)) rc.move(dir);
+                    }
+                    // Move and attack randomly if no objective.
+                    Direction dir = directions[rng.nextInt(directions.length)];
+                    MapLocation nextLoc = rc.getLocation().add(dir);
+                    if (rc.canMove(dir)){
+                        rc.move(dir);
+                    }
+                    else if (rc.canAttack(nextLoc)){
+                        rc.attack(nextLoc);
+                        System.out.println("Take that! Damaged an enemy that was in our way!");
+                    }
+
+                    // Rarely attempt placing traps behind the robot.
+                    MapLocation prevLoc = rc.getLocation().subtract(dir);
+                    if (rc.canBuild(TrapType.EXPLOSIVE, prevLoc) && rng.nextInt() % 37 == 1)
+                        rc.build(TrapType.EXPLOSIVE, prevLoc);
+                    // We can also move our code into different methods or classes to better organize it!
+                    updateEnemyRobots(rc);
+                }
 
             } catch (GameActionException e) {
                 // Oh no! It looks like we did something illegal in the Battlecode world. You should
                 // handle GameActionExceptions judiciously, in case unexpected events occur in the game
                 // world. Remember, uncaught exceptions cause your robot to explode!
-                System.out.println(rc.getType() + " Exception");
+                System.out.println("GameActionException");
                 e.printStackTrace();
 
             } catch (Exception e) {
                 // Oh no! It looks like our code tried to do something bad. This isn't a
                 // GameActionException, so it's more likely to be a bug in our code.
-                System.out.println(rc.getType() + " Exception");
+                System.out.println("Exception");
                 e.printStackTrace();
 
             } finally {
@@ -92,5 +133,23 @@ public strictfp class RobotPlayer {
         }
 
         // Your code should never reach here (unless it's intentional)! Self-destruction imminent...
+    }
+    public static void updateEnemyRobots(RobotController rc) throws GameActionException{
+        // Sensing methods can be passed in a radius of -1 to automatically 
+        // use the largest possible value.
+        RobotInfo[] enemyRobots = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+        if (enemyRobots.length != 0){
+            rc.setIndicatorString("There are nearby enemy robots! Scary!");
+            // Save an array of locations with enemy robots in them for future use.
+            MapLocation[] enemyLocations = new MapLocation[enemyRobots.length];
+            for (int i = 0; i < enemyRobots.length; i++){
+                enemyLocations[i] = enemyRobots[i].getLocation();
+            }
+            // Let the rest of our team know how many enemy robots we see!
+            if (rc.canWriteSharedArray(0, enemyRobots.length)){
+                rc.writeSharedArray(0, enemyRobots.length);
+                int numEnemies = rc.readSharedArray(0);
+            }
+        }
     }
 }
